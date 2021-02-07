52bb28f22fdf5f63cb5eb8b6a204eed16dcc63d6
maisieo <maisieobb@googlemail.com>
2021-02-07 18:33:57 +0000

    Working

--- src/Components/Response.js
+++ src/Components/Response.js
@@ -2,58 +2,57 @@ import React from "react";
 import "./Response.css";
 
 function Response(props) {
-    // let w = props.weather;
-    // console.log(props.weather.name)
+   //define all the variables I will need
     let w = props.forecast;
-    let unfilteredHours = w.forecast.forecastday[0].hour
-    let entries = Object.entries(unfilteredHours)
+    let optimumTime = ""; // the time at which to walk
+    let optimumTimeMessage = ""; // final message to show
+    let lateMessage = ""; // additional message if time is late
+    let startTime = null; // time after which walk should be
+    let weatherConditionsAtTime = ""; // weather conditions at time of walk
+    let unfilteredHours = w.forecast.forecastday[0].hour // first fetch of all hours
+    let entries = Object.entries(unfilteredHours) // returns an array of these hours
     console.log("initial grab", entries)
-  
-    // filter out the information we need from the api. 
-    // these are all hour objects with no filter on them
-
-    // finds the current time and sunset hours for the particular day
-    let timeOfAccess = (Number(w.location.localtime.slice(11,-3)));
+      let timeOfAccess = (Number(w.location.localtime.slice(11,-3))); //finds and converts time of access
     console.log("time of A", timeOfAccess)
-    let sunsetHours = (Number(w.forecast.forecastday[0].astro.sunset.slice(0,2)) + 12);
+    let sunsetHours = (Number(w.forecast.forecastday[0].astro.sunset.slice(0,2)) + 12); // finds and converts sunset
     let sunriseHours = (Number(w.forecast.forecastday[0].astro.sunrise.slice(0,2)));
-    console.log("Sunrise hours", sunriseHours)
-    let optimumTimeMessage = "";
-    let startTime = null;
-    if (timeOfAccess < sunriseHours) {
-        startTime = sunriseHours
-    }
+    console.log("Sunrise hours", sunriseHours) // finds and converts sunrise
+    let sunnyHours = []; //these are the sunny hours in a day
+    let dryHours = []; //these are dry hours in a day
+    let rainyDayHours = [] //hours to use on a rainy day (all)
 
-    else {
-        startTime = timeOfAccess;
-    }
-    console.log(startTime)
+    /**** DEFINES THE START TIME *****/ 
+    if (timeOfAccess < sunriseHours) {startTime = sunriseHours}
+    //if time of access is before sunrise, set the start time as sun rise
+    else {startTime = timeOfAccess;} // or start time is the time of access
+    console.log("This is the start time -->", startTime)
+    console.log("This is the time of access", timeOfAccess)
+    console.log("This is the sunsetHours", sunsetHours)
 
-    // filter out hours after sunrise and before sunset
-if (timeOfAccess < sunsetHours) {
-    let eligbleHours = [];
+
+    /*** SETS THE SUNNY HOURS ***/
+    if (timeOfAccess < sunsetHours) { //this if statement ends at the bottom
     for (let i=0; i < entries.length; i++) {
-    if (i > startTime && i < sunsetHours) eligbleHours.push(entries[i])}
-    console.log("These are eligibleHours", eligbleHours)
+    if (i > startTime && i < sunsetHours) sunnyHours.push(entries[i])}
+    console.log("These are sunny hours -->", sunnyHours)
 
-    // filter out hours that are not going to rain   
-    let dryHours = [];
-    for (let i=0; i < eligbleHours.length; i++){
+ 
+    /***FINDS DRY HOURS***/
+    for (let i=0; i < sunnyHours.length; i++){
          { 
-             for (let j=0; j < eligbleHours[i].length; j++) {
-            if (eligbleHours[i][j].will_it_rain === 0) {
-                dryHours.push(eligbleHours[i])
+             for (let j=0; j < sunnyHours[i].length; j++) {
+            if (sunnyHours[i][j].will_it_rain === 0) {
+                dryHours.push(sunnyHours[i])
             }
          }
       }
     }
-// If there are no dry hours, reset array to eligible hours
-     let rainyDayHours = []   
-    if (dryHours.length === 0) {
-            rainyDayHours = eligbleHours;
-            }
- 
-// This block of code takes the array above with a length of more than 0 and sorts the objects by temperature
+
+    // *** IF NO DRY HOURS, ALL HOURS ARE "DRY" HOURS***//   
+    if (dryHours.length === 0) rainyDayHours = dryHours;
+        
+
+    //**** SORTS HOURS INTO TEMERATURE TYPE *** //
     let comfortableTemp = [];
     let coldTemp = [];
     let veryHot = [];
@@ -61,140 +60,165 @@ if (timeOfAccess < sunsetHours) {
     let rainyComfortable = [];
     let rainyCold = [];
 
-    if (rainyDayHours.length === 0) {
-        for (let i=0; i < dryHours.length; i++){
+    if (rainyDayHours.length === 0) { //if not going to rain all day
+        for (let i=0; i < dryHours.length; i++){ // loop through dry hours array to find right hour
             { 
                 for (let j=0; j < dryHours[i].length; j++) {
                if (dryHours[i][j].temp_c > 10 & dryHours[i][j].temp_c <= 20) {
-                   comfortableTemp.push(dryHours[i])
+                   comfortableTemp.push(dryHours[i]) // if temp between 10 and 20, push to comfortable array
                }
                else if (dryHours[i][j].temp_c < 10) {
-                   coldTemp.push(dryHours[i])
+                   coldTemp.push(dryHours[i]) // if temp less than 10, push to coldtemp array
                }
                else if (dryHours[i][j].temp_c > 20) {
-                   veryHot.push(dryHours[i]);
+                   veryHot.push(dryHours[i]); // if temp more than 20, push to veryhot array
                }
             }
          }
        }
     }   
     else {
-        for (let i=0; i < rainyDayHours.length; i++){
+        for (let i=0; i < rainyDayHours.length; i++){ //if whole day is rainy
             { 
                 for (let j=0; j < rainyDayHours[i].length; j++) {
                if (rainyDayHours[i][j].temp_c > 10 & rainyDayHours[i][j].temp_c <= 20) {
-                   rainyComfortable.push(rainyDayHours[i])
+                   rainyComfortable.push(rainyDayHours[i]) //push temps between 10 and 20 to rainycomfortable
                }
                else if (rainyDayHours[i][j].temp_c <= 10) {
-                   rainyCold.push(rainyDayHours[i])
+                   rainyCold.push(rainyDayHours[i]) // push temps < 10 to rainycold
                }
                else if (rainyDayHours[i][j].temp_c > 20) {
-                   rainyHot.push(rainyDayHours[i]);
+                   rainyHot.push(rainyDayHours[i]); // push temps > 20 to rainyhot
                }
             }
          }
        }
     }   
 
-// those arrays need to now be sorted
-// temp between 10 and 20 and dry: lowest humidity
+/***SORT TEMP ARRAYS BY A CONDITION ***/
 
 let sortedComfortable = comfortableTemp.sort(function(a,b){
-    return a[1].humidity - b[1].humidity
-})
-// // lowest wind speed (makes more of a difference than a few degrees in temp)
+     return a[1].humidity - b[1].humidity //sort by lowest humidity
+   })
+console.log("This is sorted comfortable", sortedComfortable);
 let sortedCold = coldTemp.sort(function(a,b){
-    return a[1].wind_mph - b[1].wind_mph
+    
+    return a[1].wind_mph - b[1].wind_mph // sort by lowest wind speed 
 })
+console.log("This is sorted cold", sortedCold);
 
-// // highest wind speed = (makes more of a difference than a few degrees in temp)
 let sortedHot = veryHot.sort(function(a,b){
-    return b[1].wind_mph - a[1].wind_mph
+    return b[1].wind_mph - a[1].wind_mph // sort by highest wind speed
 })
-// //lowest temp
+console.log("This is sorted hot", sortedHot);
+
 let sortedRainyHot = rainyHot.sort(function(a,b){
-    return a[1].temp_c - b[1].temp_c
+    console.log("This is sorted rainy hot", sortedRainyHot);
+    return a[1].temp_c - b[1].temp_c // sort by lowest temp
 })
-// //lowest percip_mm
+
 let sortedRainyComfortable = rainyComfortable.sort(function(a,b){
-    return a[1].totalprecip_mm - b[1].totalprecip_mm
+    console.log("This is sorted rainy comfortable", sortedRainyComfortable);
+    return a[1].totalprecip_mm - b[1].totalprecip_mm //sort by lowest percipitation
 })
 
-// //lowest percip_mm
 let sortedRainyCold =  rainyCold.sort(function(a,b){
-    return a[1].totalprecip_mm - b[1].totalprecip_mm;
+    console.log("This is sorted rainy cold", sortedRainyCold);
+    return a[1].totalprecip_mm - b[1].totalprecip_mm; //sort by lowest percipitation
 })
 
-// defining the optimum time
-let optimumTime = 0;
-// ordered by preference
-    // checks comfortable dry array first
+/**** DEFINING OPTIMUM TIME TO WALK ****/
+
+// checks comfortable dry array first
 if (sortedComfortable.length > 0) {
-    if (Number(sortedComfortable[0][0]) > 12)  {
-    optimumTime = (Number(sortedComfortable[0][0])-12 + ":00 p.m.")
+    if (Number(sortedComfortable[0][0]) > 12)  { //if time is greater than 12, take twelve away and add "pm"
+    optimumTime = (Number(sortedComfortable[0][0])-12 + ":00 p.m.") 
+    //if it has entires, set optimum time to the first entry in the array
 }
-    else (optimumTime  = (Number(sortedComfortable[0][0]) + ":00 a.m"))
+    else (optimumTime  = (Number(sortedComfortable[0][0]) + ":00 a.m")) // or add am
+
 }
- //now checks cold and dry array
+
+//if no entries in comfortable array, check sorted cold array
 else if (sortedCold.length > 0) {
     if (Number(sortedCold[0][0]) > 12)  {
-    optimumTime  = (Number(sortedCold[0][0])-12 + ":00 p.m.")
+    optimumTime  = (Number(sortedCold[0][0])-12 + ":00 p.m.")//if time is greater than 12, take twelve away and add "pm"
+       //if it has entires, set optimum time to the first entry in the array
     }
-    else (optimumTime  = (Number(sortedCold[0][0]) + ":00 a.m"))
+    else (optimumTime  = (Number(sortedCold[0][0]) + ":00 a.m")) //or am
 }
 
-//now checks dry and hot
+//now checks dry and hot array
 else if (sortedHot.length > 0) {
     if (Number(sortedHot[0][0]) > 12)  {
-    optimumTime  = (Number(sortedHot[0][0])-12 + ":00 p.m.")
+    optimumTime  = (Number(sortedHot[0][0])-12 + ":00 p.m.")//if time is greater than 12, take twelve away and add "pm"
+     //if it has entires, set optimum time to the first entry in the array
     }
-    else (optimumTime  = (Number(sortedHot[0][0]) + ":00 a.m"))
+    else (optimumTime  = (Number(sortedHot[0][0]) + ":00 a.m"))//or "am"
 }
 
 // now checks comfortable rainy
 else if (sortedRainyComfortable.length > 0) {
-    if (Number(sortedRainyComfortable[0][0]) > 12)  {
+    if (Number(sortedRainyComfortable[0][0]) > 12)  {//if time is greater than 12, take twelve away and add "pm"
     optimumTime  = (Number(sortedRainyComfortable[0][0])-12 + ":00 p.m.")
+    //if it has entires, set optimum time to the first entry in the array
     }
-    else (optimumTime  = (Number(sortedRainyComfortable[0][0]) + ":00 a.m")) 
+    else (optimumTime  = (Number(sortedRainyComfortable[0][0]) + ":00 a.m")) //or "am"
+    
 }
 // now checks hot and rainy
 else if (sortedRainyHot.length > 0) {
     if (Number(sortedRainyHot[0][0]) > 12)  {
-    optimumTime  = (Number(sortedRainyHot[0][0])-12 + ":00 p.m.")
-    }
-    else (optimumTime  = (Number(sortedRainyHot[0][0]) + ":00 a.m"))
+    optimumTime  = (Number(sortedRainyHot[0][0])-12 + ":00 p.m.") //if time is greater than 12, take twelve away and add "pm"
+    } //if it has entires, set optimum time to the first entry in the array
+    else (optimumTime  = (Number(sortedRainyHot[0][0]) + ":00 a.m"))//or "am"
 }
 
 //now checks cold and rainy
 else if (sortedRainyCold.length > 0) {
     if (Number(sortedRainyCold[0][0]) > 12)  {
-    optimumTime  = (Number(sortedRainyCold[0][0])-12 + ":00 p.m.")
+    optimumTime  = (Number(sortedRainyCold[0][0])-12 + ":00 p.m.")//if time is greater than 12, take twelve away and add "pm"
+    } //if it has entires, set optimum time to the first entry in the array
+    else (optimumTime  = (Number(sortedRainyCold[0][0]) + ":00 a.m"))//or "am"
+}
+
+console.log("This is the optimum time -->", optimumTime)
+
+/*** SHOWS DIFFERENT MESSAGE DEPENDING ON TIME OF DAY */
+if  (optimumTime === 0) { //if there is no optimum time, it must be after sunset
+    optimumTimeMessage = "Tomorrow."
+    lateMessage = "It's too late for a walk."
+    weatherConditionsAtTime = ""; //no weather conditions show
+    
+}
+else {optimumTimeMessage = optimumTime}
+
+//otherwise the optimum time message shown is the optimum time
+
     }
-    else (optimumTime  = (Number(sortedRainyCold[0][0]) + ":00 a.m"))
-}
 
+ else {
+    optimumTimeMessage = "Tomorrow."
+ lateMessage = "It's too late for a walk."
+ weatherConditionsAtTime = "";
 
-if  (optimumTime === 0) {
-    optimumTimeMessage = "It's too late for a walk. Try again tomorrow, unless you have a torch."
-}
-else {optimumTimeMessage = "You should take your walk at " + optimumTime}
+ console.log("This is the optimum time message", optimumTimeMessage)
 
-}
- else {optimumTimeMessage = "It's too late for a walk. Try again tomorrow, unless you have a torch."}
-
+ }
 
 return (
         <div className="Response"> 
               
                                       
-              <p> {optimumTimeMessage}</p>
+              <p> The perfect time for your walk is:<br>
+              </br> <span id="time"> {optimumTimeMessage}</span><br></br> {lateMessage}</p>
               <p></p>
                
         </div>
     )
-    
-}
+} 
+
+
 
 export default Response;
 
