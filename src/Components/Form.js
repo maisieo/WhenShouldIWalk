e323119c38657a67f0cccc00c1096f0dbe4808bd
maisieo <maisieobb@googlemail.com>
2021-02-14 15:41:37 +0000

    with react-router

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,52 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Collapsible from "react-collapsible";
-import "./Form.css"
+import "./Form.css";
 
- function Form(props) {
-    //  define the initial use state of the form
-    const [location, setLocation] = useState("");
+function Form(props) {
+  //  define the initial use state of the form
+  const [location, setLocation] = useState("");
 
-    const handleChange = e => {
+  const handleChange = (e) => {
     // handle key presses
-    setLocation(e.target.value)
+    setLocation(e.target.value);
     // e is the event that receives the event, which has a property of value
     // sets the location field
-    };
+  };
 
-    const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     props.onSubmit(location);
     setLocation("");
     // resets to empty string
-       
-    }
-     return(
-     <div className="CityForm">
-         <form onSubmit={handleSubmit}>
-            <label>
-                Location
-                <input
-                 type="text"
-                 name="location"
-                 value={location}
-                 onChange={handleChange}
-                 placeholder="I live in..."
-                  />
-                 {/* Don't want a submit button, need to find a way to submit just with "enter" */}
-                 
-            </label>
-            </form>
-            
-            <Collapsible trigger="Click here for advanced settings" id="advancedSettings">
-                
-            <form class="settings-body">
-            <label for="startTime">I can get out between the hours of <input id="startTime" type="time"></input></label>
-            <label for="endTime"> and <input id="endTime" type="time"></input></label>
-            
-            </form>
-            </Collapsible>
-     </div>
-     )
- }
- 
- export default Form;
+  };
+  return (
+    <div className="CityForm">
+      <h2> When should I take my walk? </h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Location
+          <input
+            type="text"
+            name="location"
+            value={location}
+            onChange={handleChange}
+            placeholder="I live in..."
+          />
+          {/* Don't want a submit button, need to find a way to submit just with "enter" */}
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
--- src/Components/WalkList.js
+++ src/Components/WalkList.js
@@ -1,29 +1,19 @@
-import React from "react"
+import React from "react";
 
 function WalkList(props) {
-    return (
-        <div className="WalkList">
-            
-            <h2>My walks</h2>
-            
-            <ul>
-                {
-                   props.walks.length > 0 && props.walks.map(t => (
-                        <li 
-                        
-                            key={t.date}
-                            
-                            >
-                                {/* list-style: none; */}
-                            
-                            {t.title} on {t.date} at {t.time} 
-                                                   </li>
-                    ))
-                }
-            </ul>
-            
-        </div>
-    );
+  return (
+    <div className="WalkList">
+      <h2>My walks</h2>
+      <ul>
+        {props.walks.length > 0 &&
+          props.walks.map((t) => (
+            <li key={t.date}>
+              {t.title} on {t.date} at {t.time}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default WalkList;
