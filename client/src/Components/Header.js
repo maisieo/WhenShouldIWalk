bc5c7d439bbbd6caf2f987db67136d54cfd97374
maisieo <maisieobb@googlemail.com>
2021-02-17 18:52:25 +0000

    Before backend

---
+++ client/src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav className="Navbar">
+      <ul>
+        <li>
+          <Link to="/" exact>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/mywalks">My walks</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
